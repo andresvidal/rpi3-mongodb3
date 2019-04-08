76983f39d2fb60bcbf058cb049761414d20db473
Your Name <you@example.com>
2019-04-08 10:45:29 +0200

    Fixed the restarting with exit status 100 issue by removing mongod.lock file each time the container starts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+if [ -f /data/db/mongod.lock ]; then
+    echo "Old mongod.lock file found! Removing..."
+    sudo rm /data/db/mongod.lock
+    echo "Removed successfully!"
+else
+    echo "No old mongod.lock file found, starting normally..."
+fi
+
 trap 'kill -2 1; wait 1' SIGTERM
 
 # if [ "$1" = 'mongod' ]; then
